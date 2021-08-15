699f3ee178120a1bb24be5f4f938c8710e090762
tariquevieira <tariquevieira@gmail.com>
2021-08-15 09:33:41 -0300

    Aula 3 completa

--- src/components/foundation/layout/Grid/index.js
+++ src/components/foundation/layout/Grid/index.js
@@ -3,24 +3,24 @@ import { breakpointsMedia } from '../../../../theme/utils/breakpointsMedia';
 import { propToStyle } from '../../../../theme/utils/propToStyle';
 
 const Container = styled.div`
-    width: 100%;
-    padding-right: 28px;
-    padding-left: 28px;
-    margin-right: auto;
-    margin-left: auto;
-    max-width: initial;
+  width: 100%;
+  padding-right: 28px;
+  padding-left: 28px;
+  margin-right: auto;
+  margin-left: auto;
+  max-width: initial;
 
-    ${breakpointsMedia({
+  ${breakpointsMedia({
     sm: css`
-      max-width: 576px; 
+      max-width: 576px;
     `,
     md: css`
       max-width: 768px;
       padding-right: 16px;
-      padding-left: 16px; 
+      padding-left: 16px;
     `,
     lg: css`
-      max-width: 1160px; 
+      max-width: 1160px;
     `,
     xl: css`
       max-width: 1222px;
@@ -37,6 +37,10 @@ export const Grid = {
     flex-wrap: wrap;
     margin-right: -16px;
     margin-left: -16px;
+    ${propToStyle('flex')}
+    ${propToStyle('marginLeft')}
+    ${propToStyle('marginRight')}
+    ${propToStyle('justifyContent')}
   `,
   Col: styled.div`
     padding-right: 16px;
@@ -45,101 +49,100 @@ export const Grid = {
     flex-grow: 1;
     max-width: 100%;
 
-
     ${({ value }) => {
-    if (typeof value === 'number') {
-      return css`
+      if (typeof value === 'number') {
+        return css`
           flex-grow: 0;
           flex-shrink: 0;
           flex-basis: ${(100 * value) / 12}%;
           max-width: ${(100 * value) / 12}%;
         `;
-    }
+      }
 
-    return breakpointsMedia({
-      xs: value?.xs
-        ? css`
-            flex-grow: 0;
-            flex-shrink: 0;
-            flex-basis: ${(100 * value.xs) / 12}%;
-            max-width: ${(100 * value.xs) / 12}%;
-          `
-        : '',
-      sm: value?.sm
-        ? css`
-            flex-grow: 0;
-            flex-shrink: 0;
-            flex-basis: ${(100 * value.sm) / 12}%;
-            max-width: ${(100 * value.sm) / 12}%;
-          `
-        : '',
-      md: value?.md
-        ? css`
-            flex-grow: 0;
-            flex-shrink: 0;
-            flex-basis: ${(100 * value.md) / 12}%;
-            max-width: ${(100 * value.md) / 12}%;
-          `
-        : '',
-      lg: value?.lg
-        ? css`
-            flex-grow: 0;
-            flex-shrink: 0;
-            flex-basis: ${(100 * value.lg) / 12}%;
-            max-width: ${(100 * value.lg) / 12}%;
-          `
-        : '',
-      xl: value?.xl
-        ? css`
-            flex-grow: 0;
-            flex-shrink: 0;
-            flex-basis: ${(100 * value.xl) / 12}%;
-            max-width: ${(100 * value.xl) / 12}%;
-          `
-        : '',
-    });
-  }}
-
+      return breakpointsMedia({
+        xs: value?.xs
+          ? css`
+              flex-grow: 0;
+              flex-shrink: 0;
+              flex-basis: ${(100 * value.xs) / 12}%;
+              max-width: ${(100 * value.xs) / 12}%;
+            `
+          : '',
+        sm: value?.sm
+          ? css`
+              flex-grow: 0;
+              flex-shrink: 0;
+              flex-basis: ${(100 * value.sm) / 12}%;
+              max-width: ${(100 * value.sm) / 12}%;
+            `
+          : '',
+        md: value?.md
+          ? css`
+              flex-grow: 0;
+              flex-shrink: 0;
+              flex-basis: ${(100 * value.md) / 12}%;
+              max-width: ${(100 * value.md) / 12}%;
+            `
+          : '',
+        lg: value?.lg
+          ? css`
+              flex-grow: 0;
+              flex-shrink: 0;
+              flex-basis: ${(100 * value.lg) / 12}%;
+              max-width: ${(100 * value.lg) / 12}%;
+            `
+          : '',
+        xl: value?.xl
+          ? css`
+              flex-grow: 0;
+              flex-shrink: 0;
+              flex-basis: ${(100 * value.xl) / 12}%;
+              max-width: ${(100 * value.xl) / 12}%;
+            `
+          : '',
+      });
+    }}
 
     ${({ offset }) => {
-    if (typeof offset === 'number') {
-      return css`
+      if (typeof offset === 'number') {
+        return css`
           margin-left: ${(100 * offset) / 12}%;
         `;
-    }
+      }
 
-    return breakpointsMedia({
-      xs: offset?.xs
-        ? css`
-            margin-left: ${(100 * offset.xs) / 12}%;
-          `
-        : '',
-      sm: offset?.sm
-        ? css`
-            margin-left: ${(100 * offset.sm) / 12}%;
-          `
-        : '',
-      md: offset?.md
-        ? css`
-            margin-left: ${(100 * offset.md) / 12}%;
-          `
-        : '',
-      lg: offset?.lg
-        ? css`
-            margin-left: ${(100 * offset.lg) / 12}%;
-          `
-        : '',
-      xl: offset?.xl
-        ? css`
-            margin-left: ${(100 * offset.xl) / 12}%;
-          `
-        : '',
-    });
-  }}
+      return breakpointsMedia({
+        xs: offset?.xs
+          ? css`
+              margin-left: ${(100 * offset.xs) / 12}%;
+            `
+          : '',
+        sm: offset?.sm
+          ? css`
+              margin-left: ${(100 * offset.sm) / 12}%;
+            `
+          : '',
+        md: offset?.md
+          ? css`
+              margin-left: ${(100 * offset.md) / 12}%;
+            `
+          : '',
+        lg: offset?.lg
+          ? css`
+              margin-left: ${(100 * offset.lg) / 12}%;
+            `
+          : '',
+        xl: offset?.xl
+          ? css`
+              margin-left: ${(100 * offset.xl) / 12}%;
+            `
+          : '',
+      });
+    }}
 
     ${propToStyle('display')}
     ${propToStyle('alignItems')}
     ${propToStyle('justifyContent')}
     ${propToStyle('flexDirection')}
+    ${propToStyle('paddingRight')}
   `,
 };
